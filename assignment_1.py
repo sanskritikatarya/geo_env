733aaf75e733a050d4fb8417ccc6df8b24c2708e
sanskritikatarya <51592718+sanskritikatarya@users.noreply.github.com>
2024-11-21 04:04:42 +0300

    assignment-1.py code 1

---
+++ assignment_1.py
@@ -0,0 +1,8 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import xarray as xr
+import pdb
+dset = xr.open_dataset(r"D:\Course_Data.zip\SRTMGL1_NC.003_Data")
+pdb.settrace()
