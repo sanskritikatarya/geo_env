7735edb831f428be19cf5f3885df06aa97eb8bcf
sanskritikatarya <51592718+sanskritikatarya@users.noreply.github.com>
2024-11-21 04:20:59 +0300

    changed file path

--- assignment_1.py
+++ assignment_1.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import xarray as xr
 import pdb
-dset = xr.open_dataset(r"D:\Course_Data.zip\SRTMGL1_NC.003_Data")
+import xarray as xr
+dset = xr.open_dataset(r"D:\Course_Data\SRTMGL1_NC.003_Data\N21E039.SRTMGL1_NC.nc")
 pdb.settrace()
