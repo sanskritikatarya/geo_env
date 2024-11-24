1d832df2c1f45d76303705683b70bdea69093502
sanskritikatarya <51592718+sanskritikatarya@users.noreply.github.com>
2024-11-24 21:54:38 +0100

    Code added

--- assignment_1.py
+++ assignment_1.py
@@ -5,4 +5,13 @@ import matplotlib.pyplot as plt
 import pdb
 import xarray as xr
 dset = xr.open_dataset(r"Course_data/SRTMGL1_NC.003_Data/N21E039.SRTMGL1_NC.nc")
-pdb.set_trace()
+pdb.set_trace() #breakpoint
+
+DEM = np.array(dset.variables['SRTMGL1_DEM'])
+dset.close()
+pdb.set_trace() #breakpoint
+#display DEM
+plt.imshow(DEM)
+cbar = plt.colorbar()
+cbar.set_label('Elevation (m asl)')
+plt.show()
