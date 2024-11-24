5de7b839c2213d9b43ed6c20e34c8f9bc75956d3
sanskritikatarya <51592718+sanskritikatarya@users.noreply.github.com>
2024-11-24 20:24:36 +0100

    slashes changes

--- assignment_1.py
+++ assignment_1.py
@@ -4,5 +4,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pdb
 import xarray as xr
-dset = xr.open_dataset(r"D:\\Course_Data\\SRTMGL1_NC.003_Data\\N21E039.SRTMGL1_NC.nc")
+dset = xr.open_dataset(r"D:/Course_Data/SRTMGL1_NC.003_Data/N21E039.SRTMGL1_NC.nc")
 pdb.settrace()
